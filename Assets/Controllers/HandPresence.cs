e877b2edfac7bfe394293ecc4a09385c709f385f
Jesper Jalvemo <jjalvemo@gmail.com>
2021-03-25 20:55:23 +0100

    rotate claw

--- Assets/Controllers/HandPresence.cs
+++ Assets/Controllers/HandPresence.cs
@@ -16,65 +16,57 @@ public class HandPresence : MonoBehaviour
     
     void Start()
     {
-        List<InputDevice> devices = new List<InputDevice>();
-        //InputDeviceCharacteristics rightCharacteristics = InputDeviceCharacteristics.Right |  InputDeviceCharacteristics.Controller;
-        //InputDeviceCharacteristics leftCharacteristics = InputDeviceCharacteristics.Left |  InputDeviceCharacteristics.Controller;
-       
-
-        InputDevices.GetDevices(devices);
-
-
-         //left = devices.Find(device => device.characteristics == InputDeviceCharacteristics.Left);
-        left = devices.Find(device => {
-            return device.characteristics.HasFlag(InputDeviceCharacteristics.Left);
-        });
-        right = devices.Find(device => device.characteristics.HasFlag(InputDeviceCharacteristics.Right));
-
-        Debug.Log("X left:" + left.name);
-        Debug.Log("X right:" + right.name);
-
-
-        foreach (var item in devices)
-        {
-            Debug.Log(item.name + item.characteristics);
-        }
         
     }
     // Update is called once per frame
     void Update()
     {
-
-        left.TryGetFeatureValue(CommonUsages.gripButton, out bool leftGripNew);
-        right.TryGetFeatureValue(CommonUsages.gripButton, out bool rightGripNew);
+        if (!left.isValid  || !left.isValid) {
+            List<InputDevice> devices = new List<InputDevice>();
+            InputDevices.GetDevices(devices);
         
-        left.TryGetFeatureValue(CommonUsages.secondaryButton, out bool leftSecondary);
-        right.TryGetFeatureValue(CommonUsages.secondaryButton, out bool rightSecondary);
+            left = devices.Find(device => device.characteristics.HasFlag(InputDeviceCharacteristics.Left));
+            right = devices.Find(device => device.characteristics.HasFlag(InputDeviceCharacteristics.Right));
+            
+        } else {
+            left.TryGetFeatureValue(CommonUsages.gripButton, out bool leftGripNew);
+            right.TryGetFeatureValue(CommonUsages.gripButton, out bool rightGripNew);
+            
+            left.TryGetFeatureValue(CommonUsages.secondaryButton, out bool leftSecondary);
+            right.TryGetFeatureValue(CommonUsages.secondaryButton, out bool rightSecondary);
 
 
+            if (leftSecondary || rightSecondary) {
+                Debug.Log("secondary");
+            }
 
-        if (leftSecondary || rightSecondary) {
-            Debug.Log("secondary");
-        }
-
-        if (leftSecondary && rightSecondary && !restart) {
-            restart = true;
-            //Debug.Log("restart");
-            //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
-        if (restart && (!leftSecondary || !rightSecondary)) {
-            restart = false;
-        }
+            if (leftSecondary && rightSecondary && !restart) {
+                restart = true;
+                //Debug.Log("restart");
+                //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            if (restart && (!leftSecondary || !rightSecondary)) {
+                restart = false;
+            }
 
 
-        if (leftGrip != leftGripNew) {
-            leftGrip = leftGripNew;
-            //Debug.Log("left grip:" + leftGrip);
-        }
-        
-        if (rightGrip != rightGripNew) {
-            rightGrip = rightGripNew;
-            //Debug.Log("right grip:" + rightGrip);
+            if (leftGrip != leftGripNew) {
+                leftGrip = leftGripNew;
+                Debug.Log("left grip:" + leftGrip);
+            }
+            
+            if (rightGrip != rightGripNew) {
+                rightGrip = rightGripNew;
+                Debug.Log("right grip:" + rightGrip);
+            }
         }
     }
+    public bool isGrippingRight() {
+        return rightGrip;
+    }
+    public bool isGrippingLeft() {
+        return leftGrip;
+    }
+
 }
 
---
+++ Assets/RotateZ.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class RotateZ : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool right = true;
+    public HandPresence handPresence;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (right && handPresence.isGrippingRight()) {
+            transform.Rotate(new Vector3(0.0f, 0.0f, 200.0f * Time.deltaTime));
+        }
+        if (!right && handPresence.isGrippingLeft()) {
+            transform.Rotate(new Vector3(0.0f, 0.0f, 200.0f * Time.deltaTime));
+        }
+    }
+}
